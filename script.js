6159636756ca458f571190313b2d29aa0f9df437
prasoonsharma9 <prasoonsharmajnv97@gmail.com>
2025-04-20 01:20:32 +0530

    till pricing section

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 function handleClick(){
   alert("You have clicked try for free");
 }
+// HERO SECTION IS ENDED HERE..
+
+// VIDEO SECTION START
+const videoContainer = document.querySelector(".video-container");
+const video = document.getElementById("videoFeature");
+const playIcon = document.getElementById("playIcon");
+
+
+video.addEventListener("click", () => {
+  if(video.paused){
+    video.play();
+    videoContainer.classList.remove("paused");
+  }
+  else{
+    video.pause();
+    videoContainer.classList.add("paused");
+  }
+});
+
+playIcon.addEventListener("click", ()=>{
+  video.play();
+  videoContainer.classList.remove("paused")
+});
+
+
+
+
+
+
+
+
+
+
